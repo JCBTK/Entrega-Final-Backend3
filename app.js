09b2217a9afcb15f8018e93e796f90c8d78a6abc
JCBTK <59676003+JCBTK@users.noreply.github.com>
2025-02-08 00:42:48 -0300

    cambios

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-import express from 'express';  
-import mongoose from 'mongoose';  
-import userRouter from './src/routes/userRouter.js';  
-import passport from 'passport';  
-import cookieParser from 'cookie-parser';  
-import dotenv from 'dotenv';  
-import cartRouter from './src/routes/cartRouter.js';    
-import connectDB from './src/config/db.js';  
-import logger from './src/utils/logger.js';  
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './src/routes/userRouter.js';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import cartRouter from './src/routes/cartRouter.js';
+import connectDB from './src/config/db.js';
+import logger from './src/utils/logger.js';
 
-dotenv.config();  
+dotenv.config();
 
-const app = express();  
+const app = express();
 
-mongoose.set('strictQuery', false);  
+mongoose.set('strictQuery', false);
 
-const url = 'mongodb://127.0.0.1:27017/ecomerc';  
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/ecomerc';
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })  
-    .then(() => console.log('Conectado a MongoDB'))  
-    .catch(err => console.error('Error de conexión a MongoDB:', err));  
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
 
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true }));  
-app.use(cookieParser());  
-app.use(passport.initialize());  
-app.use('/api/users', userRouter);  
-app.use('/api/carts', cartRouter);  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use('/api/users', userRouter);
+app.use('/api/carts', cartRouter);
 
-const PORT = process.env.PORT || 8080;  
-app.listen(PORT, () => {  
-    console.log(`Servidor iniciado en el puerto ${PORT}`);  
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
